0326b62d06212e1f094e4625464d702164c9aeab
tangbaobao <tangbaoabo@nefu.edu.cn>
2018-06-16 19:18:05 +0800

    tangxuejun 增加实体类

---
+++ src/main/java/cn/edu/nefu/domain/Profession.java
@@ -0,0 +1,17 @@
+package cn.edu.nefu.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author 唐学俊
+ * @create 2018/06/16
+ **/
+@Getter
+@Setter
+@ToString()
+public class Profession {
+    private int professionId;
+    private String professionName;
+}
